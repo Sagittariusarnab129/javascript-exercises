5d7d22d627ca54406cc8622a68409920e9e1be96
Sagittarius_a_rnab <arnab.bose.22@aot.edu.in>
2023-07-25 23:31:32 +0530

    Added upto reverse_string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word ,num) {
+let sum = '';
+if(num<0){
+    return 'ERROR';
+}else if(num === 0){
+return '';
+}else{
+for(let i=1;i<=num;i++){
+    sum += word;
+}
+}
+return sum;
 };
 
 // Do not edit below this line
